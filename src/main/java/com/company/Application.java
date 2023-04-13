47b6dcc1e0f436d99bdcaadfe79c67bf75b7efba
Amanda Skinner <amandamuscha@yahoo.com>
2023-04-13 17:06:05 -0400

    need to prune access token from response string

--- src/main/java/com/company/Application.java
+++ src/main/java/com/company/Application.java
@@ -10,9 +10,10 @@ public class Application {
     public static void main(String[] args) {
 	// write your code here
         ApiDataService apidataservice = new ApiDataService();
-        Scanner scan = new Scanner(System.in);
+        System.out.println(apidataservice.fetchAuthToken());
+        /*Scanner scan = new Scanner(System.in);
         System.out.println("Please enter a city");
         String cityquery = scan.nextLine();
-        System.out.println(apidataservice.getCityList(cityquery));
+        System.out.println(apidataservice.getCityList(cityquery));*/
     }
 }
--- src/main/java/com/company/services/ApiDataService.java
+++ src/main/java/com/company/services/ApiDataService.java
@@ -1,23 +1,27 @@
 package com.company.services;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.commons.codec.binary.Base64;
+
+import java.nio.charset.Charset;
 
 public class ApiDataService {
     public static String API_AUTH_URL = "https://test.api.amadeus.com/v1/security/oauth2/token";
     public static String API_CLIENT_ID = "segAQY78SxIY5wcNSDsj1ADuLQ2IvvgD";
     public static String API_CLIENT_SECRET = "SQTS5J8SBblkgvqG";
-    public static String API_ACCESS_TOKEN = "wGZpR7JzBuFFv92yzgyi1vThVTNb";//Expires every 30 min
+    public static String API_ACCESS_TOKEN = "1D9k0xiAtpYgii6dTAEiVPJWb7PU";//Expires every 30 min
     private RestTemplate restTemplate = new RestTemplate();
 
     public String getAuthorization() {
-
         return restTemplate.getForObject("https://test.api.amadeus.com/v1/security/oauth2/token/" + API_ACCESS_TOKEN, String.class);
     }
 
-    public String getCityList(String keyword){
+    public String getCityList(String keyword) {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(API_ACCESS_TOKEN);
         HttpEntity<String> entity = new HttpEntity<String>(headers);
@@ -25,6 +29,20 @@ public class ApiDataService {
                 + "&page[limit]=5";
         String body = restTemplate.exchange(url, HttpMethod.GET, entity, String.class).getBody();
         return body;
+    }
+
+    public String fetchAuthToken() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        String auth = API_CLIENT_ID + ":" + API_CLIENT_SECRET;
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+        String authHeader = "Basic " + new String(encodedAuth);
+        headers.set("Authorization", authHeader);
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+        map.add("grant_type", "client_credentials");
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+        ResponseEntity<JsonNode> response = restTemplate.exchange(API_AUTH_URL, HttpMethod.POST, request, JsonNode.class);
+        return response.toString();
     }
 }
 
