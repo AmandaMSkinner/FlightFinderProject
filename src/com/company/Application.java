d66eabdcb9483520094887ae257986947e398609
Amanda Skinner <amandamuscha@yahoo.com>
2023-04-13 16:20:32 -0400

    Added restTemplate functionality

--- src/com/company/Application.java
+++ src/com/company/Application.java
@@ -3,11 +3,16 @@ package com.company;
 import com.company.model.ApiData;
 import com.company.services.ApiDataService;
 
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
 	// write your code here
         ApiDataService apidataservice = new ApiDataService();
-        apidataservice.getApiData("yell");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a city");
+        String cityquery = scan.nextLine();
+        System.out.println(apidataservice.getCityList(cityquery));
     }
 }
--- src/com/company/services/ApiDataService.java
+++ src/com/company/services/ApiDataService.java
@@ -1,22 +1,35 @@
 package com.company.services;
 
-import com.company.model.ApiData;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
 public class ApiDataService {
-    public static String API_BASE_URL = "https://developer.nps.gov/api/v1/parks";
-    public static String API_KEY = "ZxjL0eqjhdM3pwzbtKCHnXMMmvLVa1RkXwOyv1mu";
+    public static String API_AUTH_URL = "https://test.api.amadeus.com/v1/security/oauth2/token";
+    public static String API_CLIENT_ID = "segAQY78SxIY5wcNSDsj1ADuLQ2IvvgD";
+    public static String API_CLIENT_SECRET = "SQTS5J8SBblkgvqG";
+    public static String API_ACCESS_TOKEN = "wGZpR7JzBuFFv92yzgyi1vThVTNb";//Expires every 30 min
     private RestTemplate restTemplate = new RestTemplate();
 
-    public ApiData getApiData(String query) {
-        try {
-            String dataJSON = restTemplate.getForObject(API_BASE_URL + "?parkCode=" + query + "&api_key=" + API_KEY, String.class);
-            ApiData apiData = new ApiData();
-            System.out.println(dataJSON);
-            return apiData;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
+    public String getAuthorization() {
+
+        return restTemplate.getForObject("https://test.api.amadeus.com/v1/security/oauth2/token/" + API_ACCESS_TOKEN, String.class);
+    }
+
+    public String getCityList(String keyword){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(API_ACCESS_TOKEN);
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        String url = "https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT,CITY&keyword=" + keyword
+                + "&page[limit]=5";
+        String body = restTemplate.exchange(url, HttpMethod.GET, entity, String.class).getBody();
+        return body;
     }
 }
+
+/*
+curl "https://test.api.amadeus.com/v1/security/oauth2/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "grant_type=client_credentials&client_id=segAQY78SxIY5wcNSDsj1ADuLQ2IvvgD&client_secret=SQTS5J8SBblkgvqG"
+ */
