41a1c009fbda557d78fa3424ac3dff2d48fedccf
Jeremy Price <pricejb75@gmail.com>
2023-05-11 11:18:39 -0400

    Added search hotel by city and stars method

--- TakeOff/src/main/java/com/techelevator/TakeOff/services/HotelApiService.java
+++ TakeOff/src/main/java/com/techelevator/TakeOff/services/HotelApiService.java
@@ -45,5 +45,17 @@ public class HotelApiService extends ApiBaseService {
         return response.getBody().getHotels();
     }
 
+    public List<Hotel> getHotelsByCityAndStars(String city, String stars) {
+        HttpEntity<String> entity = new HttpEntity<String>(getHeadersWithAuth());
+
+        String part1 = "https://test.api.amadeus.com/v1/reference-data/locations/hotels/by-city?ratings=" + stars;
+
+        String part2 = "&cityCode=" + city;
+
+        ResponseEntity<HotelData> response =
+                restTemplate.exchange(part1 + part2, HttpMethod.GET, entity, HotelData.class);
+        return response.getBody().getHotels();
+    }
+
 
 }
