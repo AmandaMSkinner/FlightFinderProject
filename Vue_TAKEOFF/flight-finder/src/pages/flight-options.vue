c02df4a914a8b84df95877cbceb19c8df0306649
Amanda Skinner <amandamuscha@yahoo.com>
2023-07-26 17:53:03 -0400

    working on flight offers

--- Vue_TAKEOFF/flight-finder/src/pages/flight-options.vue
+++ Vue_TAKEOFF/flight-finder/src/pages/flight-options.vue
@@ -1,10 +1,32 @@
 <template>
-  
+<div>
+{{flightOffers}}    
+</div>  
 </template>
 
 <script>
+import FlightService from "../services/FlightService.js";
 export default {
-
+    data(){
+        return{
+            flightOffers: [],
+        }
+    },
+    created:{
+        getFlights(){
+            FlightService.getFlights(
+                this.store.state.flightPreferencesDto.originLocationCode,
+                this.store.state.flightPreferencesDto.destinationLocationCode,
+                this.store.state.flightPreferencesDto.departureDate,
+                this.store.state.flightPreferencesDto.returnDate,
+                this.store.state.flightPreferencesDto.adults
+            ).then(
+                response=>{
+                    this.flightOffers=response.data
+                }
+            )
+        }
+    }
 }
 </script>
 
